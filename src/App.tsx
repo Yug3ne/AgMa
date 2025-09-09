46915a22d7801617803d98e6409fb956fd8d9832
Yug3ne <eugenekoech9@gmail.com>
2025-09-09 08:37:43 +0300

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router";
+import Landing from "./pages/landing";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Service from "./pages/service";
+import Assessment from "./pages/assessment";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Landing/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="/service" element={<Service/>}/>
+      <Route path="/assessment" element={<Assessment/>}/>
+    </Routes>
+  )
 };
 
 export default App;
